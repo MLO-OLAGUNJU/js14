634df9c5e9a8b0882469c78839d8f3904e31d84b
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-18 21:49:06 +0100

    learning the scopes of javascripts

--- js/main.js
+++ js/main.js
@@ -12,9 +12,9 @@ console.log(x); */
 x = 2; // won't work coz it's constant
 console.log(x); */
 
-//global scope
+/* //global scope
 var x = 1;
-// let y = 2;
+let y = 2;
 // const z = 3;
 
 //we have block scope and fucntion local scope
@@ -22,7 +22,7 @@ var x = 1;
 //local scope //------------ function scope (declared inside a function)
 function myFunc() {
   const z = 5;
-  console.log(z);
+  console.log(y);
 
   // local scope //------------ block scope (declared inside a code block {})
   {
@@ -31,4 +31,40 @@ function myFunc() {
   }
 }
 
+myFunc(); */
+
+//global scope
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(`global: ${x}`);
+console.log(`global: ${y}`);
+console.log(`global: ${z}`);
+
+function myFunc() {
+  var x = 10;
+  const z = 5;
+  {
+    var x = 11; // function scoped
+    const z = 6; // block scoped
+    console.log(`block: ${x}`);
+    console.log(`block: ${y}`);
+    console.log(`block: ${z}`);
+  }
+  console.log(`function: ${x}`);
+  console.log(`function: ${y}`);
+  console.log(`function: ${z}`);
+}
+
 myFunc();
+
+// Global = Not in function or block, Not desirable.
+// Local = in a function or block. Not global.
+// var instantiates function() scoped variables.
+//let and const instantiate {block} scoped variables.
+
+//--------------===Helpful Tips=====---------------------
+//1. Avoid using var. Stick to using const and let.
+//2. Use const unless you need to reassign value.
+//3. Use let if you know you will reassign value.
