e41d77197ef9bdd3e54f82d139943802ed0e1552
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-18 20:30:10 +0100

    learning the scopes of javascripts

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// var, let, and const
+
+/* var x = 1;
+var x = 2;
+console.log(x); */
+
+/* let x = 1;
+x = 2;
+console.log(x); */
+
+/* const x = 1;
+x = 2; // won't work coz it's constant
+console.log(x); */
