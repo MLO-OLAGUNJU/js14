2b349be68ef0d19beb6d1071ecff131391c719c8
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-18 20:55:38 +0100

    learning the scopes of javascripts

--- js/main.js
+++ js/main.js
@@ -11,3 +11,19 @@ console.log(x); */
 /* const x = 1;
 x = 2; // won't work coz it's constant
 console.log(x); */
+
+//global scope
+var x = 1;
+let y = 2;
+const z = 3;
+
+//we have block scope and fucntion local scope
+// local scope //------------ block scope (declared inside a code block {})
+{
+  let y = 4;
+}
+
+//local scope //------------ function scope (declared inside a function)
+function myFunc() {
+  const z = 5;
+}
